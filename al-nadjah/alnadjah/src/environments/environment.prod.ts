515611cbb18b9358920d8c559e6509d283cadb3d
unknown <momogafou85@gmail.com>
2022-10-15 23:33:16 +0200

    alnadjah/shop-app:add ressources url for deployment

--- al-nadjah/alnadjah/src/environments/environment.prod.ts
+++ al-nadjah/alnadjah/src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: true,
   serverUrl: {
-    products: 'dit/shop-app/assets/api/productsAllFour.json',
-    categories: 'dit/shop-app/assets/api/categories.json',
+    products:
+      'https://alnadjah-shop-app.web.app/dist/shop-app/assets/api/productsAllFour.json',
+    categories:
+      'https://alnadjah-shop-app.web.app/dist/shop-app/assets/api/categories.json',
   },
   siteName: 'Alnadjah',
   siteEmail: 'contact@gmdt.fr',
