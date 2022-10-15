ae138cc2acbfaa3b88cced8a32a8f351bb0cadac
unknown <momogafou85@gmail.com>
2022-10-15 23:41:21 +0200

    alnadjah/shop-app:config app for deployment

--- al-nadjah/alnadjah/src/environments/environment.prod.ts
+++ al-nadjah/alnadjah/src/environments/environment.prod.ts
@@ -2,9 +2,8 @@ export const environment = {
   production: true,
   serverUrl: {
     products:
-      'https://alnadjah-shop-app.web.app/dist/shop-app/assets/api/productsAllFour.json',
-    categories:
-      'https://alnadjah-shop-app.web.app/dist/shop-app/assets/api/categories.json',
+      'https://alnadjah-shop-app.web.app/assets/api/productsAllFour.json',
+    categories: 'https://alnadjah-shop-app.web.app/assets/api/categories.json',
   },
   siteName: 'Alnadjah',
   siteEmail: 'contact@gmdt.fr',
